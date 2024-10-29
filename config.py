eb6de521b18cc73f515aeb933589478913335382
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-10-29 23:48:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-git clone https://github.com/TroJanzHEX/Unlimited-Filter-Bot
+git https://github.com/TroJanzHEX/Unlimited-Filter-Bot
 cd Unlimited-Filter-Bot
 pip3 install -r requirements.txt
 # <Create config.py appropriately>
