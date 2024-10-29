e80beb3e6b3662d2d91defa48da4905ee4e3a6ea
sahiljangrax <157376542+sahiljangrax@users.noreply.github.com>
2024-10-29 23:33:31 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+git clone https://github.com/sahiljangrax/TroJanzHEX-Unlimited-Filter-Bot
+cd TroJanzHEX-Unlimited-Filter-Bot
+pip3 install -r requirements.txt
+# <Create config.py appropriately>
+python3 bot.py
